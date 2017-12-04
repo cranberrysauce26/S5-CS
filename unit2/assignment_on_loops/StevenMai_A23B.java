823d53e0561c48f3f5a35262d48d3806680e3f5a
Steven Mai <stevenmai132@yahoo.ca>
2017-12-04 18:46:43 -0500

    misunderstood bonus question this entire time

--- unit2/assignment_on_loops/StevenMai_A23B.java
+++ unit2/assignment_on_loops/StevenMai_A23B.java
@@ -1,29 +1,48 @@
 /*input
-53 31
+79 130
 */
 import java.util.*;
 
 public class StevenMai_A23B {
 
-	static void recurse(int t1, int t2, Queue<Integer> q) {
-		if (t1-t2<0) System.out.println(q.size());
-		else{
-			q.add(t1-t2);
-			recurse(t2, t1-t2, q);
-			System.out.printf("%d ", q.poll());
-		} 
+	static void display(int t1, int t2) {
+		System.out.printf("%d %d ", t1, t2);
+		for (; t1-t2 >= 0;) {
+			int tmp = t1-t2;
+			t1 = t2;
+			t2 = tmp;
+			System.out.printf("%d ", t2);
+		}
+		System.out.println();
 	}
 
-	static void display(int t1, int t2) {
-		Queue<Integer> q = new ArrayDeque<Integer>();
-		q.add(t1); q.add(t2);
-		recurse(t1, t2, q);
-		System.out.printf("%d %d\n", q.poll(), q.poll());
+	static int getLength(int t1, int t2) {
+		int f1 = 1, f2 = 1;
+
+		int n, sign=-1;
+
+		for (n = 3; sign*(f2*t2-f1*t1) >= 0; ++n, sign*=-1) {
+			int tmp = f1+f2;
+			f1 = f2;
+			f2 = tmp;
+		}
+
+		/*
+			T(n) = -1^n ( f(n-2) * t2 - f(n-3) * t1)
+
+			T(3) = T(1) - T(2)
+			T(4) = 2*T(2)-T(1)
+			T(5) = 2 t1 - 3 t2
+
+		*/
+
+		return n-1;
 	}
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		int t1 = scan.nextInt(), t2 = scan.nextInt();
+		System.out.println(getLength(t1, t2));
 		display(t1, t2);
 		scan.close();
 	}
