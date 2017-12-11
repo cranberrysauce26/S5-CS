1779f52fca85acc363ef4478607a2c6750133df1
Steven Mai <stevenmai132@yahoo.ca>
2017-12-10 22:11:57 -0500

    comments for 12

--- unit3/3.1/StevenMai_3112.java
+++ unit3/3.1/StevenMai_3112.java
@@ -8,8 +8,22 @@ public class StevenMai_3112 {
 	static final int LOG = 25; 
 
 	public static void main(String[] args) {
+
+		/*
+			This program does a binary search to find
+			the last x such that fact(x) resuls in a stack overflow error.
+
+			We do this by trying to set the first LOG bits of x.
+			All the other bits are zero because we are assuming that x < 2^LOG.
+
+			Therefore we try to set the k'th bit. If it does not give us an error,
+			we turn that bit on. Otherwise, we leave it off.
+
+			Finally we return x.
+		*/
+
 		int x = 0;
-		for (int k = LOG; k >= 0; --k) {
+		for (int k = LOG-1; k >= 0; --k) {
 			try {
 				fact(x += (1<<k));
 			} catch(StackOverflowError e) {
