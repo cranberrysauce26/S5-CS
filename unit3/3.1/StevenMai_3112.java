70979bdcae7b5e29f5ca259a2dcee959ea2e8f2a
Steven Mai <stevenmai132@yahoo.ca>
2017-12-10 22:07:34 -0500

    binary search for question 12

--- unit3/3.1/StevenMai_3112.java
+++ unit3/3.1/StevenMai_3112.java
@@ -3,7 +3,19 @@ import java.util.*;
 public class StevenMai_3112 {
 	static long fact(int n) {return n==0 ? 1 : n * fact(n-1);}
 
+	// Choose LOG so that 2^LOG is gauranteed to give you an error
+	// 2^25 is very large and will almost certainly give a stack overflow error
+	static final int LOG = 25; 
+
 	public static void main(String[] args) {
-		System.out.println(fact(10000));
+		int x = 0;
+		for (int k = LOG; k >= 0; --k) {
+			try {
+				fact(x += (1<<k));
+			} catch(StackOverflowError e) {
+				x -= (1<<k);
+			}
+		}
+		System.out.println(x);
 	}
 }
