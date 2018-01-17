5355de5fb8a3907466a13c092be50fdbfcddc1fa
Steven Mai <stevenmai132@yahoo.ca>
2018-01-17 14:58:19 -0500

    Ianine's partition algorithm is better than mine

--- unit3/Sorting/QuickSort.java
+++ unit3/Sorting/QuickSort.java
@@ -75,9 +75,59 @@ public class QuickSort {
 		}
 	}
 
+	/*
+		Proof of correctness of partition2
+		
+		Invariants:
+		for all x in [oldL, l) A[x] < A[r]
+		for all x in (r, oldR] A[x] >= A[r]
+
+		Initialization: 
+
+			trivially true.
+
+		Maintenance
+
+			If the first case executes than this invariant clearly is satisfied.
+			Else then the net effect before decrementing r is that 
+			
+			A[l] goes to position r
+			A[r-1] goes to position l
+			A[r] goes to position r-1.
+			Therefore now A[r-1] <= A[r]
+			And so for all x in [r, oldR] A[x] >= A[r-1]
+			Therefore we decrement r and maintain the invariants.
+
+		Termination.
+
+			r-l decreases by 1 every time and so it must terminate.
+			At termination, l==r. 
+			So all oldL <= x < l have A[x] < A[r] and all l < y <= oldR A[y] >= A[l].
+			
+	*/
+
+	static int partition2(int A[], int l, int r) {
+		// if l > r throw error
+		while (l < r) {
+			if (A[l] < A[r]) ++l;
+			else {
+				int tmp = A[l];
+				A[l] = A[r-1];
+				A[r-1] = A[r];
+				A[r] = tmp;
+				// Or you can do this (but it's slower)
+				// swap(A, l, r-1);
+				// swap(A, r-1, r);
+				--r;
+			}
+		}
+		return l; // or r. They will be equal
+	}
+
 	static void quickSort(int a[], int l, int r) {
 		if (l>=r) return;
-		int p = partition(a, l, r, rand.nextInt(r-l)+l);
+		// int p = partition(a, l, r, rand.nextInt(r-l)+l);
+		int p = partition2(a, l, r); // better
 		quickSort(a, l, p-1);
 		quickSort(a, p+1, r);
 	}
@@ -86,7 +136,7 @@ public class QuickSort {
 		rand = new Random();
 		int N = 10000;
 		int[] a = new int[N];
-		for (int i = 0; i < N; ++i) a[i] = rand.nextInt(10+1);
+		for (int i = 0; i < N; ++i) a[i] = rand.nextInt(10000+1);
 		quickSort(a, 0, a.length-1);
 		for (int i = 0; i + 1 < a.length; ++i) {
 			if (a[i+1] < a[i]) {
